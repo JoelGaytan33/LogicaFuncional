03e5d5b374002c9ebbede182a80caef490aec17d
Joel Gaytan Mendez <71796345+JoelGaytan33@users.noreply.github.com>
2020-11-11 18:04:49 -0700

    practica1

---
+++ Funcional/Introduccion/Practica1.hs
@@ -0,0 +1,36 @@
+--OPERADORES MATEMATICOS
+--sumas
+suma1 a b c = a + b + c
+suma2 a b c d e = a + b + c + d + e
+--restas
+resta1 a b c = a - b - c
+resta2 a b c d = a - b - c - d
+--multiplicacion
+multiplicacion1 a b = a * b
+multiplicacion2 a b c d = a * b * c * d
+--division
+division1 a b = a / b
+division2 a b c = a / b / c
+--REALIZA UNA DIVISION
+div1 a b = div a b
+div2 a b c = div a b c
+--RESIDUO DE UNA DIVISION
+mod1 a b = mod a b
+mod2 a b c = mod a b c
+--TRUE AND FALSE
+mayor a b = a > b
+menor a b = a < b
+--AND Y OR
+--
+and1 a b = a && b
+or1 a b = a || b
+--COMPARACION: IGUAL, DIFERENTE 
+igual x y = x == y
+diferente x y = x /= y
+--NEGACION
+--
+pregunta a = not a 
+--CONCATENAR CADENAS
+--
+concatenar a b c d = ab ++ cd
+
